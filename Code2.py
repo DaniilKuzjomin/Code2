65947be1c4b3552db6cc34b7e728201670019246
Opilane TTHK <opilane@tthk.ee>
2021-11-09 16:19:24 +0200

    Add project files.

---
+++ Code2.py
@@ -0,0 +1,39 @@
+#7 Задача
+#from random import *
+##A<B
+#A=randint(10,100)
+#B=randint(100,1000)
+#K=int(input("K: "))
+#for i in range(A,B+1):
+#    if i%K==0: print(i)
+
+#for x in range(10): #0-9...
+#    print("Hello world!")
+#for x in range(1,11): #1-10...
+#    print("Hello world!")
+#for x in range(start,stop,step): #0...N-1
+#    print("Hello world!")
+
+#1 Задача
+#p=0
+#for i in range(15): #0,1,2,3...
+    #A=0
+    #while type(A)!=float:
+        #try:
+            #A=float(input(f"Sisesta {i+1} arv -> "))
+        #except ValueError:
+            #print("Oi")
+
+    #if A==round(A): #Округление
+       #p+=1
+        #print(str(A)+" on täisarv") #Выводит, что число целое
+    #else:
+        #print(str(A)+" ei ole täisarv") #Выводит, что число не целое
+#print(str(p)+" Täisarvud") #Выводит сколько целых чисел получилось
+
+#4 Задача
+#for i in range(10,21):
+#    print(i**2)
+
+
+
